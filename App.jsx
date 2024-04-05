4c5a0b7e3cb07eb7a7add7171e32155501e520fe
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-04-05 16:50:10 +0400

    05042

--- App.jsx
+++ App.jsx
@@ -1,63 +1,9 @@
-import React, { useState } from 'react';
-import {
-    View,
-    Text,
-    TextInput,
-    StyleSheet,
-    Button,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/navigations/AppNavigation';
 
-const LoginForm = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleLogin = () => {
-        // Implement login logic here
-        console.log('Username:', username, 'Password:', password);
-    };
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Login</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Username"
-                onChangeText={setUsername}
-                value={username}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={setPassword}
-                value={password}
-            />
-            <Button title="Login" onPress={handleLogin} />
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#876918',
-        flex: 1,
-        justifyContent: 'center',
-        paddingHorizontal: 45,
-    },
-    title: {
-        fontSize: 34,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    input: {
-        height: 50,
-        borderColor: 'black',
-        borderWidth: 1,
-        paddingHorizontal: 10,
-        marginBottom: 10,
-    },
-});
-
-export default LoginForm;
-
+export default function App() {
+  return (
+     <AppContainer />
+  );
+}
