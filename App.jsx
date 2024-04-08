9a3162a8147d69ab8c87e43cddbde05c1e3a5c49
Elnur Bayramzada <elnurbzade@yahoo.com>
2024-04-08 14:18:48 +0400

    0804

--- App.jsx
+++ App.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppContainer from './src/navigations/AppNavigation';
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Button,
+} from 'react-native';
 
-export default function App() {
-  return (
-     <AppContainer />
-  );
-}
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        
+        console.log('Username:', username, 'Password:', password);
+    };
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.title}>EA Camp</Text>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                onChangeText={setUsername}
+                value={username}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={setPassword}
+                value={password}
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'italic',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+    },
+});
+
+export default LoginForm;
